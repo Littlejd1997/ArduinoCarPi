4bee740beab59748e98c0512921a7c9329b56523
Jon Schober <jondavid@schober.org>
2015-06-07 03:19:29 +0000

    idek

--- ArduinoCarPi.py
+++ ArduinoCarPi.py
@@ -13,6 +13,9 @@ class Arduino:
 		self.B = B
 		self.socket.write("1 "+str(R)+" "+str(G)+" "+str(B)+"\n");
 	def lightRGB(self,R,G,B):
+		R = 255 if (R > 255) else R 
+		G = 255 if (G > 255) else G
+		B = 255 if (B > 255) else B
 		message = "2 "+str(int(R))+" "+str(int(G))+" "+str(int(B))+"\n"
 		self.socket.write(message);
 		print message;
